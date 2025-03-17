1abd2023e0863869bece29a1ae1458ca7d8d6158
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-03-17 19:40:54 +0300

    Create composition_06.cpp

---
+++ composition_06.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+class Member {
+public:
+	Member()
+	{
+		std::cout << "Member default ctor.\n";
+	}
+
+	Member(const Member&)
+	{
+		std::cout << "Member copy ctor.\n";
+	}
+};
+
+class Owner {
+public:
+	Owner()
+	{
+		std::cout << "Owner default ctor.\n";
+	}
+
+	Owner(const Owner &)
+	{
+		std::cout << "Owner copy ctor.\n";
+	}
+
+	// ...
+
+private:
+	Member mx;
+};
+
+int main()
+{
+	Owner ox;
+	Owner oy = std::move(ox);
+}
+
+/*
+	Member default ctor.
+	Owner default ctor.
+	Member default ctor.
+	Owner copy ctor.
+*/
